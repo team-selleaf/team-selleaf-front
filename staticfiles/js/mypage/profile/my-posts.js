2ec3fa363832ba25f7c5f43c596117ce3ba37e76
NoviceKim <k7679568@naver.com>
2024-02-15 11:40:22 +0900

    added my-classes menu on pages in mypage

--- staticfiles/js/mypage/profile/my-posts.js
+++ staticfiles/js/mypage/profile/my-posts.js
@@ -32,3 +32,20 @@ document.addEventListener("click", (e) => {
   }
   // 상위 요소가 모달일 경우(모달 클릭한 경우) 아무 것도 실행 안함(상태 유지)
 });
+
+/*
+  강사 여부에 따라 강의 현황 메뉴 표시/숨김
+*/
+
+// 강사 여부
+let isTeacher = false;
+
+// 강의 현황메뉴 객체
+const myClassMenu = document.querySelector(".teacher");
+
+// 강사면 강의 현황 메뉴 표시, 아니면 숨김
+if (isTeacher) {
+  myClassMenu.style.display = "inline-block";
+} else {
+  myClassMenu.style.display = "none";
+}
