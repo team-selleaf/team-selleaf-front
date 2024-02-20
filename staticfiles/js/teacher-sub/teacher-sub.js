08fa023296e1176bc53081613c95c0b5a7e31de4
System-out-gyuil <a46884334@gmail.com>
2024-02-20 17:34:24 +0900

    중간 커밋

--- staticfiles/js/teacher-sub/teacher-sub.js
+++ staticfiles/js/teacher-sub/teacher-sub.js
@@ -1,92 +1,61 @@
-// 파일입력
-const imgFileInput = document.querySelector("#img-file");
 const prevImgBox = document.querySelector(".prev-img-box");
-const upContainer = document.querySelector(".upload-container");
+const inputs = document.querySelectorAll("input[type=file]");
 
-imgFileInput.addEventListener("change", (e) => {
-  const file = e.target.files[0];
-  console.log(file);
+inputs.forEach((input, index) => {
+  input.addEventListener("change", (e) => {
+    const targetInput = e.target;
+    const file = targetInput.files[0];
+    const reader = new FileReader();
 
-  const reader = new FileReader();
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-  reader.addEventListener("load", (e) => {
-    const path = e.target.result;
+    reader.onload = (event) => {
+      const path = event.target.result;
+      e.target.nextElementSibling.setAttribute("src", path);
+      e.target.closest(".prev-img-box-item").style.display = "block";
 
-    // 사진 썸네일 뜨는 div
-    const imgBoxElement = document.createElement("div");
-
-    // 각각 div에 필요한 클래스 추가
-    imgBoxElement.classList.add("prev-img-box-item");
-
-    // 각각 div에 내부 요소 추가
-    // 사진 썸네일
-    imgBoxElement.innerHTML = `
-    <img
-    src="${path}"
-    alt=""
-    class="prev-img"
-    />
-    <div class="cancel-div">
-    <button type="button" class="cancel-btn">
-    <img
-    src="/staticfiles/images/cancel.png"
-    alt=""
-    class="cancel-img"/>
-    </button>
-    </div>
-    `;
-
-    prevImgBox.appendChild(imgBoxElement);
-
-    const delBtn = document.querySelectorAll(".cancel-btn");
-    const imgBoxes = document.querySelectorAll(".prev-img-box-item");
-
-    if (imgBoxes.length > 4) {
-      // 사진이 다섯장 이상일때
-
-    } else if (imgBoxes.length < 5) {
-      // 사진이 다섯만 미만일때
-
-    }
-    
-    delBtn.forEach((del, i) => {
-      del.addEventListener("click", () => {
-        imgBoxes[i].remove();
+      const label = e.target.closest(".upload-wrap").querySelector("label");
+      let count = 5;
+      inputs.forEach((item) => {
+        if (item.value === "") {
+          label.setAttribute("for", item.id);
+          count--;
+        }
       });
-    });
 
+      if (count === 5) {
+        e.target.closest(".upload-wrap").querySelector("label").style.display = "none";
+      }
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   });
 });
 
-//미리보기 이미지 스크롤 마우스로
-let isMouseDown = false;
-let startX, scrollLeft;
+function hideImageAndInput(prevBox, input) {
+  prevBox.style.display = "none";
+  input.style.display = "none";
+}
 
-prevImgBox.addEventListener("mousedown", (e) => {
-  isMouseDown = true;
-  prevImgBox.classList.add("active");
+const cancelBtns = document.querySelectorAll(".cancel-btn");
+cancelBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const prevBox = e.target.closest(".prev-img-box-item");
+    const input = prevBox.querySelector("input");
+    hideImageAndInput(prevBox, input);
+    const label = e.target.closest(".upload-wrap").querySelector("label");
 
-  startX = e.pageX - prevImgBox.offsetLeft;
-  scrollLeft = prevImgBox.scrollLeft;
-});
-
-prevImgBox.addEventListener("mouseleave", () => {
-  isMouseDown = false;
-  prevImgBox.classList.remove("active");
-});
-
-prevImgBox.addEventListener("mouseup", () => {
-  isMouseDown = false;
-  prevImgBox.classList.remove("active");
-});
-
-prevImgBox.addEventListener("mousemove", (e) => {
-  if (!isMouseDown) return;
-
-  e.preventDefault();
-  const x = e.pageX - prevImgBox.offsetLeft;
-  const walk = (x - startX) * 1;
-  prevImgBox.scrollLeft = scrollLeft - walk;
+    input.value = "";
+    let count = 5;
+    inputs.forEach((item) => {
+      console.log(item.value);
+      if (item.value == "") {
+        label.setAttribute("for", item.id);
+        count--;
+      }
+    });
+    console.log(count);
+    if (count != 5) {
+      e.target.closest(".upload-wrap").querySelector("label").style.display = "flex";
+    }
+  });
 });
